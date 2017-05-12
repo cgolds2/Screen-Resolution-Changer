23ec2b31fa630b8aa76e7651050c68ffcdf486a6
Connor Goldsmith <cgolds2@lsu.edu>
2017-05-12 00:34:00 -0500

    Doesnt Work For Second Monitor

--- ScreenResChanger/Form1.Designer.cs
+++ ScreenResChanger/Form1.Designer.cs
@@ -28,12 +28,170 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvScreens = new System.Windows.Forms.DataGridView();
+            this.ScreenNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ScreenName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ResolutionX = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ResolutionY = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGetProfiles = new System.Windows.Forms.Button();
+            this.dgvProfiles = new System.Windows.Forms.DataGridView();
+            this.ProfileNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Frequency = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.setResolution = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvScreens)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProfiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvScreens
+            // 
+            this.dgvScreens.AllowUserToAddRows = false;
+            this.dgvScreens.AllowUserToDeleteRows = false;
+            this.dgvScreens.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvScreens.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ScreenNumber,
+            this.ScreenName,
+            this.ResolutionX,
+            this.ResolutionY});
+            this.dgvScreens.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvScreens.Location = new System.Drawing.Point(12, 25);
+            this.dgvScreens.MultiSelect = false;
+            this.dgvScreens.Name = "dgvScreens";
+            this.dgvScreens.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvScreens.Size = new System.Drawing.Size(443, 150);
+            this.dgvScreens.TabIndex = 0;
+            // 
+            // ScreenNumber
+            // 
+            this.ScreenNumber.HeaderText = "Screen Number";
+            this.ScreenNumber.Name = "ScreenNumber";
+            this.ScreenNumber.ReadOnly = true;
+            // 
+            // ScreenName
+            // 
+            this.ScreenName.HeaderText = "Screen Name";
+            this.ScreenName.Name = "ScreenName";
+            this.ScreenName.ReadOnly = true;
+            // 
+            // ResolutionX
+            // 
+            this.ResolutionX.HeaderText = "Resolution X";
+            this.ResolutionX.Name = "ResolutionX";
+            this.ResolutionX.ReadOnly = true;
+            // 
+            // ResolutionY
+            // 
+            this.ResolutionY.HeaderText = "Resolution Y";
+            this.ResolutionY.Name = "ResolutionY";
+            this.ResolutionY.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Screens";
+            // 
+            // btnGetProfiles
+            // 
+            this.btnGetProfiles.Location = new System.Drawing.Point(12, 181);
+            this.btnGetProfiles.Name = "btnGetProfiles";
+            this.btnGetProfiles.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnGetProfiles.Size = new System.Drawing.Size(75, 23);
+            this.btnGetProfiles.TabIndex = 2;
+            this.btnGetProfiles.Text = "Get Profiles";
+            this.btnGetProfiles.UseVisualStyleBackColor = true;
+            this.btnGetProfiles.Click += new System.EventHandler(this.btnGetProfiles_Click);
+            // 
+            // dgvProfiles
+            // 
+            this.dgvProfiles.AllowUserToAddRows = false;
+            this.dgvProfiles.AllowUserToDeleteRows = false;
+            this.dgvProfiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProfiles.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ProfileNumber,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.Frequency});
+            this.dgvProfiles.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvProfiles.Location = new System.Drawing.Point(15, 210);
+            this.dgvProfiles.MultiSelect = false;
+            this.dgvProfiles.Name = "dgvProfiles";
+            this.dgvProfiles.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvProfiles.Size = new System.Drawing.Size(462, 438);
+            this.dgvProfiles.TabIndex = 3;
+            // 
+            // ProfileNumber
+            // 
+            this.ProfileNumber.HeaderText = "Profile Number";
+            this.ProfileNumber.Name = "ProfileNumber";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "Resolution X";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "Resolution Y";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            // 
+            // Frequency
+            // 
+            this.Frequency.HeaderText = "Frequency";
+            this.Frequency.Name = "Frequency";
+            this.Frequency.ReadOnly = true;
+            // 
+            // setResolution
+            // 
+            this.setResolution.Location = new System.Drawing.Point(483, 210);
+            this.setResolution.Name = "setResolution";
+            this.setResolution.Size = new System.Drawing.Size(160, 40);
+            this.setResolution.TabIndex = 4;
+            this.setResolution.Text = "Set Resolution";
+            this.setResolution.UseVisualStyleBackColor = true;
+            this.setResolution.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 687);
+            this.Controls.Add(this.setResolution);
+            this.Controls.Add(this.dgvProfiles);
+            this.Controls.Add(this.btnGetProfiles);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvScreens);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvScreens)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProfiles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvScreens;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ScreenNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ScreenName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ResolutionX;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ResolutionY;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGetProfiles;
+        private System.Windows.Forms.DataGridView dgvProfiles;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProfileNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Frequency;
+        private System.Windows.Forms.Button setResolution;
     }
 }
 
--- ScreenResChanger/Form1.cs
+++ ScreenResChanger/Form1.cs
@@ -18,56 +18,13 @@ namespace ScreenResChanger
         public Form1()
         {
             InitializeComponent();
+            int i = 0;
             foreach (var screen in Screen.AllScreens)
             {
                 // For each screen, add the screen properties to a list box.
-                List<String> screens = new List<String>();
-                screens.Add("Device Name: " + screen.DeviceName);
-                screens.Add("Bounds: " + screen.Bounds.ToString());
-                screens.Add("Type: " + screen.GetType().ToString());
-                screens.Add("Working Area: " + screen.WorkingArea.ToString());
-                screens.Add("Primary Screen: " + screen.Primary.ToString());
-            }
+                dgvScreens.Rows.Add(i, screen.Bounds.Size.Width, screen.DeviceName, screen.Bounds.Size.Height);
 
-            DEVMODE OSpecs = new DEVMODE();
-            getCurrentRes(ref OSpecs);
-
-            int selH = 900;
-            int selW = 1600;
-            int selF = (int)OSpecs.dmDisplayFrequency;
-            int selG = (int)OSpecs.dmDisplayFlags;
-            int selB = (int)OSpecs.dmBitsPerPel;
-            int Ondx = getDMbySpecs((int)OSpecs.dmPelsHeight, (int)OSpecs.dmPelsWidth, (int)OSpecs.dmDisplayFrequency, (int)OSpecs.dmDisplayFlags, (int)OSpecs.dmBitsPerPel, ref OSpecs);
-            Screen Srn = Screen.PrimaryScreen;
-            Console.WriteLine("Current res is " + OSpecs.dmPelsHeight + " by " + OSpecs.dmPelsWidth + "\n");
-            DEVMODE NSpecs = new DEVMODE();
-            int Nndx = getDMbySpecs(selH, selW, selF, selG, selB, ref NSpecs);
-            
-            //Note that this function sets both the DEVMODE to the selected display mode and returns the index value of this display mode. It returns -1 if it fails (-1 is the value of ENUM_CURRENT_SETTINGS), and sets the DEVMODE to the current display mode.
-            if (Nndx == -1)
-            {
-                Console.WriteLine("Could not find specified mode");
-            }
-            else if (setDisplayMode(ref NSpecs) || setDisplayMode(Nndx)) //This is just to illustrate both ways of doing it. One or the other may be more convenient (ie, the latter if you are getting this from a file, the former if you already have the DEVMODE in your program, etc.)
-            {
-                //reset display mode to original after waiting long enough to see it changed
-                Console.WriteLine("Current res is " + NSpecs.dmPelsHeight + " by " + NSpecs.dmPelsWidth + "\n");
-                Console.WriteLine("Successful change. Waiting 4 seconds.");
-                Thread.Sleep(4000);
-                if (setDisplayMode(ref OSpecs) || setDisplayMode(Ondx))
-                {
-                    //success!
-                    Console.WriteLine("Mode reversion succeeded.");
-                }
-                else
-                {
-                    Console.WriteLine("Mode reversion failed. Manual reset required.");
-                }
-            }
-            else
-            {
-                //return
-                Console.WriteLine("Resolution change failed. Aborting");
+                i++;
             }
         }
         static bool setDisplayMode(int i)
@@ -95,6 +52,7 @@ namespace ScreenResChanger
                 return false;
             }
         }
+
         static int getDMbySpecs(int H, int W, int F, int G, int B, ref DEVMODE DM)
         {
             DM.dmSize = (ushort)Marshal.SizeOf(DM);
@@ -102,19 +60,19 @@ namespace ScreenResChanger
             SelDM.dmSize = (ushort)Marshal.SizeOf(SelDM);
             int iOMI = 0;
             int i = 0;
-            
+
             for (iOMI = 0; !(EnumDisplaySettings(null, iOMI, ref SelDM) == false); iOMI++)
             {
                 Console.WriteLine(
-                    "F = " + SelDM.dmDisplayFrequency.ToString() + 
-                    " G = " + SelDM.dmDisplayFlags.ToString() + 
+                    "F = " + SelDM.dmDisplayFrequency.ToString() +
+                    " G = " + SelDM.dmDisplayFlags.ToString() +
                     " B = " + SelDM.dmBitsPerPel.ToString() +
                     " H = " + SelDM.dmPelsHeight.ToString() +
                     " W = " + SelDM.dmPelsWidth.ToString()
                     );
-                if(H==SelDM.dmPelsHeight && W == SelDM.dmPelsWidth && F== SelDM.dmDisplayFrequency)
+                if (H == SelDM.dmPelsHeight && W == SelDM.dmPelsWidth && F == SelDM.dmDisplayFrequency)
                 {
-                    B = (int) SelDM.dmBitsPerPel;
+                    B = (int)SelDM.dmBitsPerPel;
                     G = (int)SelDM.dmDisplayFlags;
                     F = (int)SelDM.dmDisplayFrequency;
                     break;
@@ -131,6 +89,7 @@ namespace ScreenResChanger
             }
             return iOMI;
         }
+
         static void getCurrentRes(ref DEVMODE dm)
         {
             dm = new DEVMODE();
@@ -395,6 +354,81 @@ namespace ScreenResChanger
             }
         }
 
+        private void btnGetProfiles_Click(object sender, EventArgs e)
+        {
+            Screen Srn = Screen.AllScreens[(int)dgvScreens.SelectedRows[0].Cells[0].Value];
+            DEVMODE OSpecs = new DEVMODE();
+            getCurrentRes(ref OSpecs);
+
+
+            OSpecs.dmSize = (ushort)Marshal.SizeOf(OSpecs);
+            DEVMODE SelDM = new DEVMODE();
+            SelDM.dmSize = (ushort)Marshal.SizeOf(SelDM);
+            int iOMI = 0;
+            int i = 0;
+            dgvProfiles.Rows.Clear();
+
+            for (iOMI = 0; !(EnumDisplaySettings(null, iOMI, ref SelDM) == false); iOMI++)
+            {
+                dgvProfiles.Rows.Add(i, SelDM.dmPelsWidth.ToString(), SelDM.dmPelsHeight.ToString(), SelDM.dmDisplayFrequency.ToString());
+                Console.WriteLine(
+                    "F = " + SelDM.dmDisplayFrequency.ToString() +
+                    " G = " + SelDM.dmDisplayFlags.ToString() +
+                    " B = " + SelDM.dmBitsPerPel.ToString() +
+                    " H = " + SelDM.dmPelsHeight.ToString() +
+                    " W = " + SelDM.dmPelsWidth.ToString()
+                    );
+
+                i++;
+            }
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DEVMODE OSpecs = new DEVMODE();
+            getCurrentRes(ref OSpecs);
+
+            int selH = 900;
+            int selW = 1600;
+            int selF = 0;
+            int selG = (int)OSpecs.dmDisplayFlags;
+            int selB = (int)OSpecs.dmBitsPerPel;
+            int Ondx = getDMbySpecs((int)OSpecs.dmPelsHeight, (int)OSpecs.dmPelsWidth, (int)OSpecs.dmDisplayFrequency, (int)OSpecs.dmDisplayFlags, (int)OSpecs.dmBitsPerPel, ref OSpecs);
+            Screen Srn = Screen.AllScreens[(int)dgvScreens.SelectedRows[0].Cells[0].Value];
+            Console.WriteLine("Current res is " + OSpecs.dmPelsHeight + " by " + OSpecs.dmPelsWidth + "\n");
+            DEVMODE NSpecs = new DEVMODE();
+            int Nndx = getDMbySpecs(selH, selW, selF, selG, selB, ref NSpecs);
+
+            //Note that this function sets both the DEVMODE to the selected display mode and returns the index value of this display mode. It returns -1 if it fails (-1 is the value of ENUM_CURRENT_SETTINGS), and sets the DEVMODE to the current display mode.
+            if (Nndx == -1)
+            {
+                Console.WriteLine("Could not find specified mode");
+            }
+            else if (setDisplayMode(ref NSpecs) || setDisplayMode(Nndx)) //This is just to illustrate both ways of doing it. One or the other may be more convenient (ie, the latter if you are getting this from a file, the former if you already have the DEVMODE in your program, etc.)
+            {
+                //reset display mode to original after waiting long enough to see it changed
+                Console.WriteLine("Current res is " + NSpecs.dmPelsHeight + " by " + NSpecs.dmPelsWidth + "\n");
+                Console.WriteLine("Successful change. Waiting 4 seconds.");
+                Thread.Sleep(4000);
+                if (setDisplayMode(ref OSpecs) || setDisplayMode(Ondx))
+                {
+                    //success!
+                    Console.WriteLine("Mode reversion succeeded.");
+                }
+                else
+                {
+                    Console.WriteLine("Mode reversion failed. Manual reset required.");
+                }
+            }
+            else
+            {
+                //return
+                Console.WriteLine("Resolution change failed. Aborting");
+            }
+        }
     }
 
 
